720787c4f21bb44eec3a34560f99e49c748b053f
VenkateshDevarakonda0706 <venkateshdevarakonda0706@gmail.com>
2025-08-26 19:17:22 +0530

    Initial commit on new-feature branch

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function page() {
-  return <div>This is a dashboard page.Still in development</div>;
+  return <div>This is a dashboard page.Still in development.Venkatesh</div>;
 }
 
 export default page;
