c7bf99ee78bd6d952f4ee7c187fd5cc3c15d1e43
VenkateshDevarakonda0706 <venkateshdevarakonda0706@gmail.com>
2025-08-26 16:39:46 +0530

    Revert "after deployment small issue of dashboard"
    This reverts commit d9b1a11abbfd36f3cfeedf1cb63da5653eddfcb5.

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,4 +1,3 @@
-/*
 import { credential, ServiceAccount } from "firebase-admin";
 import { App, getApp, getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
@@ -19,30 +18,6 @@ if (!getApps().length) {
 }
 
 // Get Firestore instance
-const adminDb = getFirestore(adminApp);
-
-export { adminApp, adminDb };
-*/
-import { credential, ServiceAccount } from "firebase-admin";
-import { App, getApp, getApps, initializeApp } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
-
-let adminApp: App;
-
-// Initialize Firebase Admin only once
-if (!getApps().length) {
-  adminApp = initializeApp({
-    credential: credential.cert({
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      projectId: process.env.FIREBASE_PROJECT_ID,
-    } as ServiceAccount),
-  });
-} else {
-  adminApp = getApp();
-}
-
-// Firestore instance
 const adminDb = getFirestore(adminApp);
 
 export { adminApp, adminDb };
